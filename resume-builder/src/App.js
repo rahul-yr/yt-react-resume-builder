e8fb5355a6358c0d10fd234de893f439a108f18c
rahul-yr <yarragodularahulreddy@gmail.com>
2022-05-20 19:09:34 +0530

    Added Google Analytics integration

--- resume-builder/src/App.js
+++ resume-builder/src/App.js
@@ -1,10 +1,20 @@
 import Dashboard from "./resumebuilder/Dashboard";
-import { useRoutes } from "react-router-dom";
+import { useRoutes, useLocation } from "react-router-dom";
 import Templates from "./resumebuilder/Components/Templates";
 import SampleResume from "./resumebuilder/Components/SampleResume";
 import EditTemplate from "./resumebuilder/Components/EditTemplate";
+import ReactGA from "react-ga4";
+import React, { useEffect } from "react";
+
+ReactGA.initialize("G-NKH1LJ0QTT");
 
 function App() {
+  const location = useLocation();
+  useEffect(() => {
+    ReactGA.set({ page: location.pathname }); // Update the user's current page
+    ReactGA.send({ hitType: "pageview", page: location.pathname });
+  }, [location]);
+
   let element = useRoutes([
     {
       path: "/",
